d334a1099e670fcb2395e0b48ba580c1ee8a9e35
Thumpen <thomas@lund>
2012-06-06 16:50:32 +0200

    Signed-off-by: Thumpen <thomas@lund>

--- lolboll.py
+++ lolboll.py
@@ -1,10 +1,14 @@
 #!/usr/bin/python
 def lolboll():
 	"""meck meck meck"""
-	"""I am master of my pythoney domain!"
+	"""I am master of my pythoney domain!"""
 	print "LOLBOLL"
 	print "erik was here"
 
 lolboll()
+
+import trip
+
+print trip.hurr()
 
 print lolboll.__doc__
--- trip.py
+++ trip.py
@@ -1,10 +1,10 @@
-def trip(n):
+def hurr():
+	n = int(raw_input("SIFFRA!"))
+
 	lol = "ol"
 	out = "lol"
 	i = 0
 	while i < n:
 		out = out + lol
 		i = i+1
-	print out
-
-trip(6)
+	return out

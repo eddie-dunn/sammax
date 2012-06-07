bf9b10bb8b55c127d72ca5f640a2b5bf555d3d8c
Firefly <firefly@Orion.(none)>
2012-06-07 23:42:50 +0200

    Removed class definition and added reference to "dasboot.py" instead.

--- main.py
+++ main.py
@@ -1,22 +1,17 @@
 import random
-
-class DasBoot:
-	def __init__(self, spd, dmg, hp, xcoord, ycoord):
-		self.s = spd
-		self.d = dmg
-		self.h = hp
-		self.x = xcoord
-		self.y = ycoord
+from dasboot import *
 
 def main():
-	boat1 = DasBoot(10, 30, 100, 5, 5,)
-	boat2 = DasBoot(10, 15, 200, 25, 25)
+	boat1 = DasBoot(10, 100, 5, 5,)
+	boat2 = DasBoot(10, 200, 25, 25)
 		
 	print "boat1", "boat2"
-	while boat1.h and boat2.h > 0:
+	while boat1.h > 0 and boat2.h > 0:
 		print boat1.h, boat2.h
 		boat1.h = boat1.h-random.randint(0, boat2.d)
 		boat2.h = boat2.h-random.randint(0, boat1.d)
+	print boat1.h, boat2.h
+		
 
 if __name__ == '__main__':
 	main()

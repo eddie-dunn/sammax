7c8d57dbbed269c0752796e0db91d798e66e69a0
Thumpen <thomas@lund>
2012-06-10 08:44:25 +0200

    har fixat till ett mkt enkelt grafikfonster i boatgame.py. Det ar meningen att det ska vara ljud ocksa men jag far bara ett irriterande klick. Funkar det for er?

--- boatgame.py
+++ boatgame.py
@@ -1,5 +1,4 @@
-import os, sys
-import pygame
+import os, sys, pygame
 from pygame.locals import *
 import random
 from dasboot import *
@@ -8,6 +7,51 @@ if not pygame.font: print 'Warning, fonts disabled'
 if not pygame.mixer: print 'Warning, sound disabled'
 
 def main():
+	"Meckar upp ett fonster med lite skrap i"
+	
+	pygame.init()
+	
+	size = width, height = 800,800
+	backg = 0,0,0
+	
+	screen = pygame.display.set_mode(size)
+	
+	boatpic = pygame.image.load("boat.jpg")
+	
+	x = 0
+	y = 0
+	x1 = 1
+	y1 = 1
+	
+	"""Spelar ett ljud jag hittade pa www.soundbible.com
+	Bonuskommentar: om man skriver svenska vokaler i kommentarerna
+	far man felmeddelanden nar man kor"""
+	
+	sound = pygame.mixer.Sound("geese.mp3")
+	
+	sound.play()
+	
+	while 1:
+		for event in pygame.event.get():
+			if event.type == pygame.QUIT:
+				sys.exit()
+		
+		backg = x%255, y%255, ((x+y)%255)
+		screen.fill(backg)
+		screen.blit(boatpic,(x,y))
+		pygame.display.flip()
+		
+		if x == width:
+			x1 = -1
+		elif x == 0:
+			x1 = 1
+		if y == height:
+			y1 = -1
+		elif y == 0:
+			y1 = 1
+	
+		x = x+x1
+		y = y+y1
 	"Changed to list"
 	boat = [DasBoot(20, 20, 100, 5, 5,), DasBoot(10, 20, 200, 25, 25)]
 	
